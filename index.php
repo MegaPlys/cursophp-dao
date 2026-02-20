80a51f309f293f1ef1dd8cef77a7de9619f5d510
Mega <megaplaysoficial1@gmail.com>
2026-02-20 16:02:40 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,28 @@
 
 require_once("config.php");
 
-$root = new Usuario();
 
-$root->loadByID(4);
+// carrega um usuário
 
-echo $root;
+// $root = new Usuario();
+// $root->loadByID(4);
+//echo $root;
+
+// Carrega uma lista de usuários
+
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+// Carrega uma lista de usuários buscando pelo login
+
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+// Carrega um usuário usando o login e a senha
+
+$usuario = new Usuario();
+$usuario->login("root", "!@#$");
+
+echo $usuario;
 
 ?>
